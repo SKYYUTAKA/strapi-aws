8cb3a39380dcfe9252f1340f3ec01f402e8fdd18
skyyutaka <yutaka009@gmail.com>
2022-11-17 11:40:23 +0800

    installed aws-s3 upload

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,9 +2,15 @@ const path = require('path');
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi-shiftingsites-database"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "strapidatabase009"),
+
     },
     useNullAsDefault: true,
   },
